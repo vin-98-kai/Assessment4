8c1605595a1b822943304aab203ef114961402da
vin-98-kai <goh98cal@gmail.com>
2022-06-24 09:30:31 +0800

    First Commit
    Edited repo name

---
+++ covid_cases.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 09:10:20 2022
+
+@author: Calvin
+"""
+
+import os
+import pandas as pd
+
+#%% Function
+
+# Static
+DATA_PATH = os.path.join(os.getcwd(),'data','cases_malaysia_train.csv')
+
+# EDA
+# Step 1) Data Loading
+df =  pd.read_csv(DATA_PATH)
+
+
+# Step 2) Data Inspection
+# Step 3) Data Cleaning
+# Step 4) Features Selection
+# Step 5) Data Preprocessing
+
+
